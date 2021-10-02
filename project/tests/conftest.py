a6ef78c43409f291f57a66a620ee53292b285b5d
Krissla <arsavit@gmail.com>
2021-10-03 01:13:43 +0300

    routes

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -2,10 +2,10 @@ import os
 
 import pytest
 from starlette.testclient import TestClient
-
-from app.main import create_application
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
+from app.main import create_application
 
 
 def get_settings_override():
@@ -18,6 +18,21 @@ def test_app():
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
+        # testing
+        yield test_client
 
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=os.getenv("DATABASE_TEST_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
         # testing
         yield test_client
